3ac03fbe1c077c5da92f4485e0cc008c5efec1b8
zengyujiao <2780531094@qq.com>
2019-10-16 18:46:01 +0800

    删除school

--- vue/day-2/school/src/comp/header/header.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue/dist/vue'
-import template from './header.html'
-export default new Vue({
-    el: '#header',
-    data: {
-        title: 'ActionSheet'
-    },
-    template,
-})
--- vue/day-2/school/src/index.js
+++
@@ -1,5 +0,0 @@
-import 'weui'
-import './style/style.css'
-import './comp/header/header'
-import './comp/search/search'
-import './comp/mask/mask'
